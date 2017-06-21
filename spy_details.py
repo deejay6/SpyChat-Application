c7ff837589b4079777f85ab94d62373fccc1f178
deejay6 <dhgambhir@gmail.com>
2017-06-21 14:04:23 +0530

    Updated Spy Details

--- spy_details.py
+++ spy_details.py
@@ -5,4 +5,16 @@ spy = {
     'age': None,
     'rating': None,
     'spy_is_online': None,
+    'chats':  [],
+    'current_status_message': None,
+    'old_status': []
 }
+friends = {
+    'friend_of': None,
+    'name': None,
+    'salutation': None,
+    'age': None,
+    'rating': None,
+    'spy_is_online': None
+}
+friends_list = []
