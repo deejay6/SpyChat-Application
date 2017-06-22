af23b304ada966152d6f7248aa6fb5a54a4fd397
deejay6 <dhgambhir@gmail.com>
2017-06-23 03:26:11 +0530

    Updated Spy Details With Classes

--- spy_details.py
+++ spy_details.py
@@ -1,25 +1,22 @@
+class Spy:
+
+    def __init__(self, name, salutation, age, rating, spy_is_online, current_status_message, old_status, chats):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.spy_is_online = spy_is_online
+        self.current_status_message = current_status_message
+        self.old_status = old_status
+
+class Friend:
+
+    def __init__(self, name, salutation, age, rating, friend_is_online, friend_of):
+        self.name = name
+        self.salutation = salutation
+        self.age = age
+        self.rating = rating
+        self.friend_is_online = friend_is_online
+        self.friend_of = friend_of
 spy_list = []
-spy = {
-    'name': None,
-    'salutation': None,
-    'age': None,
-    'rating': None,
-    'spy_is_online': None,
-    'current_status_message': None,
-    'old_status': []
-}
-friends = {
-    'friend_of': None,
-    'name': None,
-    'salutation': None,
-    'age': None,
-    'rating': None,
-    'friend_is_online': None,
-    'chats': []
-}
-friends_list = []
-chat = {
-  'sent_by_me': None,
-  'message': None,
-  'datetime': None
-}
+friend_list = []
