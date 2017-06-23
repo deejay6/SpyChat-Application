bb90c38e7cbf4c2e5389ed5c38e833a0895cca67
deejay6 <dhgambhir@gmail.com>
2017-06-23 13:21:25 +0530

    Updated With Friend and Chat Class

--- spy_details.py
+++ spy_details.py
@@ -1,6 +1,9 @@
+from datetime import datetime
+
+
 class Spy:
 
-    def __init__(self, name, salutation, age, rating, spy_is_online, current_status_message, old_status, chats):
+    def __init__(self, name, salutation, age, rating, spy_is_online, current_status_message, old_status, friend_list):
         self.name = name
         self.salutation = salutation
         self.age = age
@@ -8,15 +11,25 @@ class Spy:
         self.spy_is_online = spy_is_online
         self.current_status_message = current_status_message
         self.old_status = old_status
+        self.friend_list = friend_list
 
 class Friend:
 
-    def __init__(self, name, salutation, age, rating, friend_is_online, friend_of):
+    def __init__(self, name, salutation, age, rating, spy_is_online, chats):
         self.name = name
         self.salutation = salutation
         self.age = age
         self.rating = rating
-        self.friend_is_online = friend_is_online
-        self.friend_of = friend_of
+        self.spy_is_online = spy_is_online
+        self.chats = chats
+
+
 spy_list = []
-friend_list = []
+
+
+class Chat:
+
+    def __init__(self, message, sent_by_me):
+        self.message = message
+        self.time = datetime.now()
+        self.sent_by_me = sent_by_me
